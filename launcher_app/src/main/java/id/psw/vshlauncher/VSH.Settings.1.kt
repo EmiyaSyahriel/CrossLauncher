2f66ef2a8d0ddfb89c2e878586454d7fc39e6248
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2022-03-23 09:06:54 +0700

    Added Wave Wallpaper Layer to setting Split-naming settings source file

--- launcher_app/src/main/java/id/psw/vshlauncher/VSH.Settings.1.kt
+++ launcher_app/src/main/java/id/psw/vshlauncher/VSH.Settings.1.kt
@@ -1,13 +1,20 @@
 package id.psw.vshlauncher
 
+import android.annotation.SuppressLint
+import android.app.WallpaperManager
+import android.content.ComponentName
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Build
+import id.psw.vshlauncher.livewallpaper.XMBWaveWallpaperService
 import id.psw.vshlauncher.submodules.GamepadSubmodule
 import id.psw.vshlauncher.types.XMBItem
 import id.psw.vshlauncher.types.items.*
+import id.psw.vshlauncher.views.VshViewPage
 import id.psw.vshlauncher.views.XMBLayoutType
 import id.psw.vshlauncher.views.XmbView
 import id.psw.vshlauncher.views.dialogviews.TestDialogView
+import id.psw.vshlauncher.views.dialogviews.TextDialogView
 import id.psw.vshlauncher.views.showDialog
 import java.util.*
 import kotlin.collections.ArrayList
@@ -321,14 +328,50 @@ private fun VSH.createCategoryDisplay() : XMBSettingsCategory {
     }
 }
 
+@SuppressLint("ApplySharedPref")
 fun VSH.createCategoryWaveSetting(): XMBSettingsCategory {
+    val vsh = this
     return XMBSettingsCategory(this,
         SettingsCategoryID.CATEGORY_SETTINGS_WAVE,
         R.drawable.category_shortcut,
         R.string.title_activity_wave_wallpaper_setting,
         R.string.empty_string
     ).apply {
+        content.add(XMBSettingsItem(vsh, "settings_wave_set",
+            R.string.settings_wave_set_name, R.string.settings_wave_set_desc,
+            R.drawable.category_setting, {""}
+        ){
+            try{
+                val i = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
+                val pkg = vsh.packageName
+                val cls = XMBWaveWallpaperService::class.java.canonicalName ?:
+                "id.psw.vshlauncher.livewallpaper.XMBWaveWallpaperService"
+                i.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT, ComponentName(pkg, cls))
+                vsh.startActivity(i)
+            }catch (e:Exception){
+                vsh.postNotification(R.drawable.category_setting, "Setting unavailable","This device probably didn't support live wallpaper")
+            }
+        })
 
+        content.add(XMBSettingsItem(vsh, "settings_wave_make_internal",
+            R.string.settings_wave_make_internal_name, R.string.setting_wave_make_internal_desc,
+            R.drawable.category_settings_display, {
+                getString(vsh.useInternalWave.select(R.string.common_yes, R.string.common_no))
+            }
+        ){
+            xmbView?.showDialog(
+                TextDialogView(vsh).setData(null, "Reboot Required",
+                    "Changing the usage of Internal Wave Layer requires the launcher to be rebooted.\n"+
+                            "Would you like to change the value and reboot?")
+                    .setPositive("Reboot"){_ ->
+                        // Commit instead of apply, allow the app to save the preference before restarting
+                        pref.edit().putBoolean(PrefEntry.USES_INTERNAL_WAVE_LAYER, !vsh.useInternalWave).commit()
+                        vsh.restart()
+                    }
+                    .setNegative("Cancel"){dlg -> dlg.finish(VshViewPage.MainMenu) }
+            )
+        }
+        )
     }
 }
 
