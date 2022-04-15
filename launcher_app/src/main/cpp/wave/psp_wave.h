ff332fac6660d4064cae8d34fc0cebb9e607cc54
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2022-04-16 03:08:29 +0700

    Refactor the Live Wallpaper Native codes

--- launcher_app/src/main/cpp/wave/psp_wave.h
+++
@@ -1,9 +0,0 @@
-#ifndef H_PSP_WAVE
-#define H_PSP_WAVE
-
-void psp_resize(float w, float h);
-void psp_start();
-void psp_draw(float ms);
-void psp_destroy();
-
-#endif
--- launcher_app/src/main/cpp/wave/textures.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifndef TEXTURES_H
-#define TEXTURES_H
-#include "gl.h"
-
-extern const char siang_shades[], malam_shades[];
-void load_texture(const char* which, GLuint* to, int size = 192, int width = 48, int height = 4);
-
-#endif
