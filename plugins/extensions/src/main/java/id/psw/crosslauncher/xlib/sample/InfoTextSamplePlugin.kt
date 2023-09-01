55e942c2a9807859f6774e86ef08b65dec442a5c
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2023-09-01 15:10:04 +0700

    Delete plugins directory, already moved to extensions
    Signed-off-by: Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>

--- plugins/extensions/src/main/java/id/psw/crosslauncher/xlib/sample/InfoTextSamplePlugin.kt
+++
@@ -1,8 +0,0 @@
-package id.psw.crosslauncher.xlib.sample
-
-import id.psw.crosslauncher.xlib.XmbStatusTextPlugin
-
-class InfoTextSamplePlugin : XmbStatusTextPlugin() {
-    override val pluginId: String
-        get() = "InfoTextSample"
-}
