4eba15ea99f63361d2d89354ec805bd8fff6fe9f
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2023-09-01 15:07:42 +0700

    Fix InfoTextSampleExtension
    Signed-off-by: Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>

--- extensions/sample/src/main/java/id/psw/crosslauncher/xlib/sample/InfoTextSampleExtension.kt
+++ extensions/sample/src/main/java/id/psw/crosslauncher/xlib/sample/InfoTextSampleExtension.kt
@@ -1,8 +1,24 @@
 package id.psw.crosslauncher.xlib.sample
 
+import android.content.Context
 import id.psw.crosslauncher.xlib.XmbStatusTextExtension
 
-class InfoTextSampleExtension : XmbStatusTextExtension() {
+class InfoTextSampleExtension(host: Context, self:Context) : XmbStatusTextExtension(host, self) {
+    companion object {
+        private val _keys = arrayOf("sample_hello", "sample_foobar")
+    }
+
     override val pluginId: String
         get() = "InfoTextSample"
+
+    override val keys: Array<out String>
+        get() = _keys
+
+    override fun getString(key: String): String? {
+        return when(key){
+            _keys[0] -> "Hello World"
+            _keys[1] -> "Foo Bar"
+            else -> "(Not Supported)"
+        }
+    }
 }
