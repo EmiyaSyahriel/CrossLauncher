f9c27c2e91847d096be2f1fa5fd7097253d84e88
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2023-09-08 11:59:20 +0700

    Use Coroutine Instead of Thread
    Signed-off-by: Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>

--- launcher_app/src/main/java/id/psw/vshlauncher/VSH.Settings.1.kt
+++ launcher_app/src/main/java/id/psw/vshlauncher/VSH.Settings.1.kt
@@ -14,6 +14,8 @@ import id.psw.vshlauncher.types.VideoIconMode
 import id.psw.vshlauncher.types.items.*
 import id.psw.vshlauncher.views.XmbLayoutType
 import id.psw.vshlauncher.views.dialogviews.*
+import id.psw.vshlauncher.views.formatStatusBar
+import id.psw.vshlauncher.views.showDialog
 import kotlinx.coroutines.*
 import java.util.*
 import kotlin.collections.ArrayList
