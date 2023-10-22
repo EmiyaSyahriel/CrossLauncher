851f8039325a76cad815590003771488e4bdce9b
Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>
2023-10-22 21:06:35 +0700

    Add ThreeTenABP & Fix Build Date Format
    Signed-off-by: Syahriel Ibnu Irfansyah <ainaaikochumpa@gmail.com>

--- launcher_app/build.gradle.kts
+++ launcher_app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins{
 
 fun getDate() : String {
     val date = Date().time
-    val fmt = SimpleDateFormat("yyyy:MM:dd:HH:mm")
+    val fmt = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
     return fmt.format(date)
 }
 
@@ -86,6 +86,8 @@ android {
 
 dependencies {
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar") )))
+
+    // Kotlin and Android Compatibility Library
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.10.1")
 
@@ -94,13 +96,25 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-process:2.4.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0")
 
+    // Date Time Compatibility Library
+    implementation("com.jakewharton.threetenabp:threetenabp:1.4.6")
+
+    // Media Library
     implementation("androidx.media:media:1.6.0")
+
+    // Animated Icon Library
     implementation("com.github.penfeizhou.android.animation:awebp:2.17.2")
     implementation("com.github.penfeizhou.android.animation:apng:2.17.2")
     implementation("com.github.penfeizhou.android.animation:gif:2.17.2")
+
+    // Multidex support (This project has >65000 methods)
     implementation("com.android.support:multidex:1.0.3")
+
+    // JSON Parsing
     implementation("com.google.code.gson:gson:2.10.1")
     implementation(project(mapOf("path" to ":launcher_xlib")))
+
+    // Test Implementation
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
