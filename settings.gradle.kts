672ee4792be69ed29478445e14ca4ef42badbad4
EmiyaSyahriel <ainaaikochumpa@gmail.com>
2023-11-24 23:02:32 +0700

    Modify Build Config to work with AndroidIDE

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,4 +16,4 @@ dependencyResolutionManagement {
 rootProject.name = "Cross Launcher"
 include(":launcher_app")
 include(":launcher_xlib")
-include(":extensions:sample")
+// include(":extensions:sample")
